fc92927a733a950618d67e9fd2188af246b80173
Mandicifu <al292486@uji.es>
2021-12-06 13:09:10 +0000

    Impl task1

---
+++ js/task1.js
@@ -0,0 +1,6 @@
+// Display the current date
+// Use Set Date to display the date of your birth
+// Convert to UTC
+const birthday = new Date()
+birthday.setFullYear(1995, 10, 02)
+alert(birthday.toUTCString())
